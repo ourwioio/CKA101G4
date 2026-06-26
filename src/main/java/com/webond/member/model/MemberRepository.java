1ecc81cae98a7c0d651879cfc88b3c09eee3a19f
Gary <ourwioioaa123@gmail.com>
2026-06-26 15:09:08 +0800

    會員、員工套件刪除

--- src/main/java/com/webond/member/model/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.webond.member.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<MemberVO, Integer> {
-
-}
