d9abcca742237253fc3fdc4f8e4dd0f8406e1d95
Gary <ourwioioaa123@gmail.com>
2026-06-28 22:59:26 +0800

    更改CharMsgVO sendMemId型別

--- src/main/java/com/webond/chat/model/ChatMsgVO.java
+++ src/main/java/com/webond/chat/model/ChatMsgVO.java
@@ -2,6 +2,8 @@ package com.webond.chat.model;
 
 import java.sql.Timestamp;
 
+import com.webond.member.model.MemberVO;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
@@ -9,39 +11,36 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
-
 @Entity
 @Table(name = "chat_message")
 public class ChatMsgVO {
-	
+
 	@Id
 	@Column(name = "MESSAGE_ID", updatable = false)
-	private Integer msgId;		
-	
+	private Integer msgId;
+
 	@ManyToOne
 	@JoinColumn(name = "SENDER_MEMBER_ID", referencedColumnName = "MEMBER_ID")
-	private Integer sendMemId;	
-	
+	private MemberVO sendMemId;
+
 	@Column(name = "CONTENT")
-	private String content;		
-	
+	private String content;
+
 	@Column(name = "SENT_AT", updatable = false)
-	private Timestamp sentAt;	
-	
+	private Timestamp sentAt;
+
 	@Column(name = "IS_READ")
-	private Integer isRead;		
-	
+	private Integer isRead;
+
 	@ManyToOne
 	@JoinColumn(name = "CHAT_ROOM_ID", referencedColumnName = "CHAT_ROOM_ID")
-	private ChatRoomVO chatRoomVO;			
-	
-
+	private ChatRoomVO chatRoomVO;
 
 	public ChatMsgVO() {
 		super();
 	}
-		
-	public ChatMsgVO(Integer msgId, Integer sendMemId, String content, Timestamp sentAt, Integer isRead,
+
+	public ChatMsgVO(Integer msgId, MemberVO sendMemId, String content, Timestamp sentAt, Integer isRead,
 			ChatRoomVO chatRoomVO) {
 		super();
 		this.msgId = msgId;
@@ -55,35 +54,43 @@ public class ChatMsgVO {
 	public Integer getMsgId() {
 		return msgId;
 	}
+
 	public void setMsgId(Integer msgId) {
 		this.msgId = msgId;
 	}
 
-	public Integer getSendMemId() {
+	public MemberVO getSendMemId() {
 		return sendMemId;
 	}
-	public void setSendMemId(Integer sendMemId) {
+
+	public void setSendMemId(MemberVO sendMemId) {
 		this.sendMemId = sendMemId;
 	}
+
 	public String getContent() {
 		return content;
 	}
+
 	public void setContent(String content) {
 		this.content = content;
 	}
+
 	public Timestamp getSentAt() {
 		return sentAt;
 	}
+
 	public void setSentAt(Timestamp sentAt) {
 		this.sentAt = sentAt;
 	}
+
 	public Integer getIsRead() {
 		return isRead;
 	}
+
 	public void setIsRead(Integer isRead) {
 		this.isRead = isRead;
 	}
-	
+
 	public void setChatRoomVO(ChatRoomVO chatRoomVO) {
 		this.chatRoomVO = chatRoomVO;
 	}
@@ -91,6 +98,5 @@ public class ChatMsgVO {
 	public ChatRoomVO getChatRoomVO() {
 		return chatRoomVO;
 	}
-
 
 }
