9118d4b4eb19ed34820a514dbde1d5b9a7e3b41c
T.H.LIN <harvey881118@gmail.com>
2026-07-14 15:02:37 +0800

    不想用了

--- src/main/java/com/webond/activity/model/ActivityOrderService.java
+++ src/main/java/com/webond/activity/model/ActivityOrderService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -28,7 +29,7 @@ public class ActivityOrderService {
 	private ActivityOrderRepository orderRepo;
 
 	public List<ActivityOrderVO> getAll() {
-		return orderRepo.findAll();
+		return orderRepo.findAll(Sort.by(Sort.Direction.DESC, "activityOrderId"));
 	}
 
 	public ActivityOrderVO getOneOrder(Integer activityOrderId) {
