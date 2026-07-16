b739394b5a2aa3d01acaa9f0d45b5b36bd94d817
Pailin <pailinliu0114@gmail.com>
2026-07-16 23:37:39 +0800

    調整平台管理後台登入權限及左側選單公告管理改為平台管理

--- src/main/java/com/webond/platform/controller/PlatformController.java
+++ src/main/java/com/webond/platform/controller/PlatformController.java
@@ -4,12 +4,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.webond.employee.model.EmployeeVO;
+
+import jakarta.servlet.http.HttpSession;
+
 @Controller
-@RequestMapping("/platform")
+@RequestMapping("/admin/platform")
 public class PlatformController {
 
 	@GetMapping("/index")
-	public String index() {
+	public String index(HttpSession session) {
+
+		EmployeeVO loginEmp = (EmployeeVO) session.getAttribute("employeeVO");
+		if (loginEmp == null) {
+			return "redirect:/admin/login";
+		}
+
 		return "back-end/platform/index";
 	}
 }
