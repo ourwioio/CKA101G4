2a50fb11026a74300e58ae2db9a6e1ea26384cad
Vreckan <ewqqwe550005@yahoo.com.tw>
2026-07-21 21:15:45 +0800

    修改場地圖片新增port映射3307

--- src/main/java/com/webond/venue/tool/VenueImageImportTool.java
+++ src/main/java/com/webond/venue/tool/VenueImageImportTool.java
@@ -9,7 +9,7 @@ import java.sql.ResultSet;
 
 public class VenueImageImportTool {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/webond_project?serverTimezone=Asia/Taipei";
+    private static final String URL = "jdbc:mysql://localhost:3307/webond_project?serverTimezone=Asia/Taipei";
     private static final String USER = "root";
     private static final String PASSWORD = "123456";
 
